274405fa2be3b042e6de5d255de2a8feb63b0e1b
havario <157877551+havario@users.noreply.github.com>
2026-08-07 22:18:49 +0800

    Normalize GeoIP2 dependency tag versions

--- scripts/ci-deps.sh
+++ scripts/ci-deps.sh
@@ -3,7 +3,7 @@
 set -eEuxo pipefail
 
 NGX_ACME_VERSION="$(gh api --paginate repos/nginx/nginx-acme/tags --jq '.[].name | sub("^v"; "")' | sort -V | tail -n 1)"
-NGX_GEOIP2_VERSION="$(gh api --paginate repos/leev/ngx_http_geoip2_module/tags --jq '.[].name' | sort -V | tail -n 1)"
+NGX_GEOIP2_VERSION="$(gh api --paginate repos/leev/ngx_http_geoip2_module/tags --jq '.[].name | sub("^v"; "")' | sort -V | tail -n 1)"
 HEADERS_MORE_VERSION="$(gh api --paginate repos/openresty/headers-more-nginx-module/tags --jq '.[].name | sub("^v"; "")' | sort -V | tail -n 1)"
 NGX_NJS_VERSION="$(gh api --paginate repos/nginx/njs/tags --jq '.[].name | sub("^v"; "")' | sort -V | tail -n 1)"
 NGX_ZSTD_VERSION="$(gh api repos/facebook/zstd/releases/latest --jq '.tag_name | sub("^v"; "")')"
